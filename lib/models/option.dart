9ff37e4555b1d08b0bb7cfcfa017be59030d2018
mk17ru <mk17.ru@mail.ru>
2021-12-08 19:54:20 +0500

    add options

---
+++ lib/models/option.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Option {
+  Image image;
+  String title;
+
+  Option({required this.image, required this.title});
+}
+
+final options = [
+  Option(image: Image.asset('assets/images/scales.jpg'), title: "First title"),
+  Option(image: Image.asset('assets/images/scales.jpg'), title: "First title"),
+  Option(image: Image.asset('assets/images/scales.jpg'), title: "First title"),
+  Option(image: Image.asset('assets/images/scales.jpg'), title: "First title"),
+
+];
