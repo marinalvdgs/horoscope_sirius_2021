a1f5fa3a57410952f649244ccdd3d1d8f7d399f0
mk17ru <mk17.ru@mail.ru>
2021-12-09 13:46:25 +0500

    add simple card

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:horoscope_sirius_2021/screens/menu/menu_screen.dart';
+import 'package:horoscope_sirius_2021/common_widgets/space_page.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Select Option UI',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primaryColor: Colors.black),
-      home: Scaffold(
-        appBar: AppBar(title: Text('ListViews')),
-        body: BodyLayout(),
+    return const MaterialApp(
+      home: SpacePage(
+        body: Center(child: Text('Space')),
       ),
     );
   }
