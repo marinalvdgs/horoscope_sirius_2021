68ec471166ede803f6a8a63c8c66f16d4513eb48
marinaivanova <h4ys-245Qe186TVZQoQa>
2021-12-13 18:36:09 +0300

    stylish comatibility screen

--- lib/screens/compatibility/compatibility_screen.dart
+++ lib/screens/compatibility/compatibility_screen.dart
@@ -2,6 +2,7 @@ import 'dart:collection';
 
 import 'package:flutter/material.dart';
 import 'package:horoscope_sirius_2021/common/enums.dart';
+import 'package:horoscope_sirius_2021/common/style.dart';
 import 'package:horoscope_sirius_2021/common_widgets/circle_wheel_view/circle_wheel_child_looping_list_delegate.dart';
 import 'package:horoscope_sirius_2021/common_widgets/circle_wheel_view/circle_wheel_scroll_view.dart';
 
@@ -21,32 +22,96 @@ class _CompatibilityScreenState extends State<CompatibilityScreen> {
   @override
   Widget build(BuildContext context) {
     return SpacePage(
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        child: Row(children: [
-          SizedBox(
-            width: MediaQuery.of(context).size.width * 0.5,
-            child: CircleScrollWidget(
-                children: allSigns
-                    .map((e) => SignCard(
-                          sign: e,
-                          direction: WheelDirection.left,
-                        ))
-                    .toList(),
-                direction: WheelDirection.left),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                icon: const Icon(Icons.arrow_back, color: Colors.white),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Совместимость', style: headerTextStyle),
+                  Text('Проверьте совместимость в любви и работе',
+                      style: subtitleTextStyle),
+                ],
+              ),
+            ],
           ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width * 0.5,
-            child: CircleScrollWidget(
-                children: allSigns
-                    .map((e) => SignCard(
-                          sign: e,
-                          direction: WheelDirection.right,
-                        ))
-                    .toList(),
-                direction: WheelDirection.right),
+          Expanded(
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Positioned.fill(
+                  child: Row(children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 12, top: 24),
+                          child: Text('Женщина', style: subtitleTextStyle),
+                        ),
+                        Flexible(
+                          child: SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.5,
+                            child: CircleScrollWidget(
+                                children: allSigns
+                                    .map((e) => SignCard(
+                                          sign: e,
+                                          direction: WheelDirection.left,
+                                        ))
+                                    .toList(),
+                                direction: WheelDirection.left),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 12, top: 24),
+                          child: Text('Мужчина', style: subtitleTextStyle),
+                        ),
+                        Flexible(
+                          child: SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.5,
+                            child: CircleScrollWidget(
+                                children: allSigns
+                                    .map((e) => SignCard(
+                                          sign: e,
+                                          direction: WheelDirection.right,
+                                        ))
+                                    .toList(),
+                                direction: WheelDirection.right),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ]),
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 96),
+                    child: FloatingActionButton.extended(
+                      onPressed: () {},
+                      label: Text(
+                        'ПРОВЕРИТЬ',
+                        style: buttonTextStyle,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
-        ]),
+        ],
       ),
     );
   }
--- lib/screens/compatibility/widgets/circle_scroll_widget.dart
+++ lib/screens/compatibility/widgets/circle_scroll_widget.dart
@@ -18,19 +18,44 @@ class CircleScrollWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Transform.translate(
-      offset: Offset(direction == WheelDirection.right ? -40 : 0, 0),
+      offset: Offset(direction == WheelDirection.right ? -30 : 0, 0),
       child: Transform(
         alignment: Alignment.center,
         transform:
             Matrix4.rotationY(direction == WheelDirection.right ? pi : 0),
-        child: CircleListScrollView.useDelegate(
-          radius: MediaQuery.of(context).size.height * 0.4,
-          itemExtent: 190,
-          clipToSize: false,
-          renderChildrenOutsideViewport: true,
-          physics: const CircleFixedExtentScrollPhysics(),
-          childDelegate:
-              CircleListWheelChildLoopingListDelegate(children: children),
+        child: Stack(
+          children: [
+            Transform.scale(
+              scale: 4.5,
+              child: Transform.translate(
+                offset: Offset(
+                    direction == WheelDirection.right
+                        ? -MediaQuery.of(context).size.width / 4
+                        : -MediaQuery.of(context).size.width / 4.3,
+                    0),
+                child: Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.height,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: const Color(0x330089BA),
+                      boxShadow: const [
+                        BoxShadow(color: Color(0x552C73D2), blurRadius: 8)
+                      ],
+                      border: Border.all(color: Color(0xFF2C73D2), width: 0.2)),
+                ),
+              ),
+            ),
+            CircleListScrollView.useDelegate(
+              radius: MediaQuery.of(context).size.height * 0.35,
+              itemExtent: 190,
+              clipToSize: false,
+              renderChildrenOutsideViewport: true,
+              physics: const CircleFixedExtentScrollPhysics(),
+              childDelegate:
+                  CircleListWheelChildLoopingListDelegate(children: children),
+            ),
+          ],
         ),
       ),
     );
